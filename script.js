9f4a2300fcd7529e36b6296aaf3569f2eab9ca76
Fabrizio Zivolo <faz94@hotmail.it>
2022-04-20 15:53:08 +0200

    corretto piccolo refuso tag img

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
 const app = new Vue({
-  el: '#root',
+  el: "#root",
   data: {
-      
-      message: 'Hello VueJS!',
-      image: 'https://picsum.photos/536/354'
-  }
+    message: "Hello VueJS!",
+    image: "https://picsum.photos/536/354",
+  },
 });
-
